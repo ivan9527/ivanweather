0bf6d4051318a37ab70b9705094f0b7ac40fa37a
ivan <418344307@qq.com>
2020-03-01 22:35:27 +0800

    加入天气显示功能

---
+++ app/src/main/java/com/example/ivanweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.ivanweather.gson;
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
--- app/src/main/java/com/example/ivanweather/util/Utility.java
+++ app/src/main/java/com/example/ivanweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.ivanweather.db.City;
 import com.example.ivanweather.db.County;
 import com.example.ivanweather.db.Province;
+import com.example.ivanweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -82,5 +84,20 @@ public class Utility {
 
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray array = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = array.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
